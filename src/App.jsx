13db0e2ce526e70437e1a059e3c6a802abf4f8f2
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-06-02 21:37:57 +0100

    Add status component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Status from "./components/Status";
 
 function App() {
 
   return (
     <>
       <Header />
+      <Status />
     </>
   );
 }
